c92c904b90a5d90a46d577f9e03fee54827cbb1d
Robot <brandonhung343@gmail.com>
2020-02-18 01:33:59 +0000

    New gridpath

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -18,11 +18,46 @@ def ramp_velocity(Vmax, t, rampTime):
     else:
         return Vmax
 
+def pid_rot_tuning_round(robot):
+    # tuning the velocity to be 0.75, w to be 0
+    Kpr = 2 
+    Kpl = 2
+    targetV = 0
+    rampTime = 0.15
+    targetW = 2 * math.pi
+    targetT = 0.2
+    nowTime = time.time()
+    startTime = nowTime
+    first = True
+    currTime = nowTime
+    powL = -70
+    powR = 70
+    while (currTime - startTime <= targetT + 2 * rampTime):
+        currTime = time.time() 
+        if (first):
+            first = False
+        else:
+            dt = currTime - nowTime
+            targetW = math.pi
+            targetW = ramp_velocity(targetW, currTime - startTime, rampTime)
+            pows = robot.pid_Vw(targetV, targetW, Kpl, Kpr, dt) 
+            robot.update_odometry(dt)
+            ''' print('Time %.3f' % (currTime - startTime))
+            print('Odom', robot.get_odometry())
+            print('Displacement of L, R', robot.get_wheel_displacement())
+            print('Enc Readgins', robot.get_encoder_readings()) '''
+            nowTime = currTime
+            # print(pows)
+            robot.drive_robot_power(powL + pows[0], powR + pows[1]) 
+        time.sleep(0.05)
+    robot.stop()
+
+
 
 def pid_rot_tuning_right(robot):
     # tuning the velocity to be 0.75, w to be 0
-    Kpr = 1
-    Kpl = 1
+    Kpr = 0.25
+    Kpl = 0.25
     targetV = 0
     rampTime = 0.15
     targetW = math.pi
@@ -31,8 +66,8 @@ def pid_rot_tuning_right(robot):
     startTime = nowTime
     first = True
     currTime = nowTime
-    powL = -40
-    powR = 40
+    powL = -42
+    powR = 42
     while (currTime - startTime <= targetT + 2 * rampTime):
         currTime = time.time() 
         if (first):
@@ -55,8 +90,8 @@ def pid_rot_tuning_right(robot):
 
 def pid_rot_tuning_left(robot):
     # tuning the velocity to be 0.75, w to be 0
-    Kpr = 1
-    Kpl = 1
+    Kpr = 0.5
+    Kpl = 0.75
     targetV = 0
     targetW = (-math.pi) 
     nowTime = time.time()
@@ -89,8 +124,8 @@ def pid_rot_tuning_left(robot):
 
 def pid_straight_tuning(robot):
     # tuning the velocity to be 0.75, w to be 0
-    Kpr = 1
-    Kpl = 1
+    Kpr = 1.5
+    Kpl = 1.5
     targetV = 4
     targetW = 0
     nowTime = time.time()
@@ -134,10 +169,10 @@ def main_loop(xstart, ystart, xgoal, ygoal, convert=False):
                 pid_straight_tuning(robot)
             time.sleep(0.35)
             if (theta == math.pi):
-                pid_rot_tuning_right(robot)
-                time.sleep(0.05)
-                pid_rot_tuning_right(robot)
-                time.sleep(0.60)
+                pid_rot_tuning_left(robot)
+                time.sleep(0.375)
+                pid_rot_tuning_left(robot)
+                time.sleep(0.375)
             elif (theta == math.pi / 2):
                 pid_rot_tuning_right(robot)
                 time.sleep(0.65)
