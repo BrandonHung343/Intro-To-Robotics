c55f87a9ac9185e2a6402e3b4f8363acdd0a9ba5
Brandon Hung <brandonhung343@gmail.com>
2020-02-22 19:04:39 -0500

    Line following with odometry to test

--- lab6/testline.py
+++ lab6/testline.py
@@ -7,6 +7,9 @@ import signal
 
 robot = rb.Robot(wheelbase=7.25, radius=1.625)
 
+def signal_handler(sig, frame):
+    robot.stop()
+    sys.exit(0)
 
 # turns to the right in order to find the line
 def find_line_right(robot):
@@ -32,7 +35,7 @@ def main():
         robot.set_sensor(2, 'light')
         time.sleep(1)
         baseLight = 2300 # figure out the line's sensor reading by observation
-        baseSpeed = [15, 15]
+        baseSpeed = [20, 20]
         # use calibrate only if checking sensor values
         # calibrate(robot)
         # lightSensor = find_line_right(robot)
@@ -41,22 +44,30 @@ def main():
         # rotates to the right
         lastErr = 0
         first = True
-        while(True):
+        nowTime = time.time()
+        startTime = nowTime
+        currTime = nowTime
+        while (currTime - startTime < 80):
+            currTime = time.time() 
+            if (first):
+                first = False
+            else:
+                dt = currTime - nowTime
+                robot.update_odometry(dt)
+                print('Time %.3f' % (currTime - startTime))
+                print('Odom', robot.get_odometry())
+                print('Displacement of L, R', robot.get_wheel_displacement())
+                print('Enc Readgins', robot.get_encoder_readings())
                 lightSensed = robot.get_sensor(2)
                 # print(lightSensed)
                 err = (lightSensed - baseLight)
-                if (first):
-                        first = False
-                        dErr = 0
-                else:
-                        dErr = lastErr - err
                 pControl = err * kp
-                dControl = dErr * kd
-                newPowLeft = baseSpeed[0] + (pControl + dControl)
-                newPowRight = baseSpeed[1] - (pControl + dControl)
+                newPowLeft = baseSpeed[0] + (pControl)
+                newPowRight = baseSpeed[1] - (pControl)
                 robot.drive_robot_power(newPowLeft, newPowRight)
-                lastErr = err
-                time.sleep(0.05)
+                nowTime = currTime
+            time.sleep(0.05)
+                
 
 if __name__ == '__main__':
         main()
